ea56e50ccccf2379108aba960ef134980d55c8a3
George Strakhov <george.strakhov@gmail.com>
2014-08-30 23:26:45 +0800

    basic scoring

--- collections/scores.js
+++ collections/scores.js
@@ -1,1 +1,20 @@
 Scores = new Meteor.Collection('scores');
+
+//allow to insert & update scores for if the user == currentUser
+Scores.allow({
+  insert: ownAndOnlyScore,
+  update: ownAndOnlyScore
+});
+
+function ownAndOnlyScore(userId, doc) {
+  //check if the user is updating or inserting for his own user
+  if(userId !== doc.user) return false;
+  //check if there is already a score for this item for this user with a different id
+  var oldScore = Scores.findOne({
+    item: doc.item,
+    user: doc.user
+  });
+  if(oldScore && (oldScore._id !== doc._id)) return false;
+  console.log(arguments);
+  return true;
+}
